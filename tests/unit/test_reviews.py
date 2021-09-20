4c2c0b5d12123992c4ef0b011c5dc1400975ebd7
s-rofe <40592658+s-rofe@users.noreply.github.com>
2021-09-20 23:36:49 +1200

    Update test_reviews.py
    updated test_reviews to reflect new data

--- tests/unit/test_reviews.py
+++ tests/unit/test_reviews.py
@@ -4,6 +4,6 @@ from library.domain.model import Book, Review
 
 
 def test_get_reviews(in_memory_repo):
-    assert len(in_memory_repo.get_reviews(27036539)) == 1
+    assert len(in_memory_repo.get_reviews(27036539)) == 4
     services.add_review(27036539, "testing-testing", 4, "test1", in_memory_repo)
-    assert len(in_memory_repo.get_reviews(27036539)) == 2
+    assert len(in_memory_repo.get_reviews(27036539)) == 5
