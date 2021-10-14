be679edf916183a52a449e028da1511e17a6be33
s-rofe <40592658+s-rofe@users.noreply.github.com>
2021-10-14 13:34:57 +1300

    Fixed requirements.txt from A2 feedback

---
+++ tests_db/unit/test_orm.py
@@ -0,0 +1,11 @@
+import pytest
+
+from sqlalchemy.exc import IntegrityError
+
+from library.domain.model import User, Book, Publisher, Author, Review
+
+def make_user():
+    pass
+
+def insert_user():
+    pass
