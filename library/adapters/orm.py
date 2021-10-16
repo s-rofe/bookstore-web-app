3891c73f734eefb38d4b28415e32f542598c507d
s-rofe <40592658+s-rofe@users.noreply.github.com>
2021-10-16 17:50:07 +1300

    Added foreign keys to tables

--- library/adapters/orm.py
+++ library/adapters/orm.py
@@ -5,57 +5,57 @@ from sqlalchemy import (
 from sqlalchemy.dialects.postgresql import ARRAY
 from sqlalchemy.orm import mapper, relationship, synonym
 
-from library.domain.model import Author, Publisher, Book, Review
+from library.domain.model import Author, Publisher, Book, Review, User
 
 
 metadata = MetaData()
 
-author_table = Table(
+authors_table = Table(
     'authors', metadata,
     Column('id', Integer, primary_key=True),
     Column('full_name', String(255), nullable=False),
-    Column('co_authors', ARRAY(Author), nullable=False)
+    Column('co_authors', ForeignKey('authors.id'))
 )
 
-book_table = Table(
+books_table = Table(
     'books', metadata,
     Column('id', Integer, primary_key=True),
     Column('title', String(255), nullable=False),
     Column('description', String(1024)),
-    Column('publisher', Publisher),
-    Column('authors', ARRAY(Author), nullable=False),
+    Column('publisher', ForeignKey('publishers.id')),
+    Column('authors', ForeignKey('authors.id')),
     Column('release_year', Integer),
     Column('ebook', Boolean),
     Column('num_pages', Integer),
     Column('total_ratings', Integer, nullable=False)
 )
 
-publisher_table = Table(
-    'publisher', metadata,
-    Column('name', String(255), primary_key=True)
+publishers_table = Table(
+    'publishers', metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('name', String(255), nullable=False)
 )
 
-review_table = Table(
+reviews_table = Table(
     'reviews', metadata,
     Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('book', Book),
+    Column('book', ForeignKey('books.id')),
     Column('review_text', String(1024), nullable=False),
     Column('rating', Integer, nullable=False),
-    Column('author', String(255)),
+    Column('author', ForeignKey('users.user_name')),
     Column('timestamp', Time, nullable=False)
 )
 
-user_table = Table(
+users_table = Table(
     'users', metadata,
     Column('id', Integer, primary_key=True, autoincrement=True),
     Column('user_name', String(255), unique=True, nullable=False),
     Column('password', String(255), nullable=False),
-    Column('read_books', ARRAY(Book), nullable=False),
-    Column('reviews', ARRAY(Review), nullable=False),
+    Column('read_books', ForeignKey('books.id')),
+    Column('reviews', ForeignKey('reviews.id')),
     Column('pages_read', Integer, nullable=False)
 )
 
 
 def map_model_to_tables():
     pass
-
